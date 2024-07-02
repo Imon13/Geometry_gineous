b4b1feb9aa8cfb0ed7ba28ca57c7a7ea9ed7ca5c
imon <imonsaha153@gmail.com>
2024-07-02 20:50:45 +0600

    paralelogram  desing and calculation are done

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -6,6 +6,9 @@ document.getElementById('btn-calculator').addEventListener('click',function(){
     const heightElement = document.getElementById('height')
     const heightString = heightElement.value
     const height = parseFloat(heightString)
+    if(isNaN(base) || isNaN(height)){
+        alert('Please insert nmber')
+    }
     const area = 0.5*base*height
     console.log(area)
     const areaspan = document.getElementById('tringle-area')
@@ -21,9 +24,29 @@ document.getElementById('btn-calculator-ractangle').addEventListener('click',fun
     const lengthElement = document.getElementById('length')
     const lengthString = lengthElement.value
     const length = parseFloat(lengthString)
+    if(isNaN(width) || isNaN(length)){
+        alert('Please insert nmber')
+    }
     const area = length*width
     const areaspan = document.getElementById('rectangle-area') 
     areaspan.innerText = area
+ 
     widthElement.value = ''
     lengthElement.value = ''
+})
+document.getElementById('btn-calculator-paralelogram').addEventListener('click',function(){
+    const paralelogramWidthElement = document.getElementById('base-paralelogram')
+    const paralelogramWidthString = paralelogramWidthElement.value
+    const paralelogramWidth = parseFloat(paralelogramWidthString)
+    const paralelogramHeightElement = document.getElementById('height-paralelogram')
+    const paralelogramHeightString = paralelogramHeightElement.value
+    const paralelogramHeight = parseFloat(paralelogramHeightString)
+    if(isNaN(paralelogramWidth) || isNaN(paralelogramHeight)){
+        alert('Please insert nmber')
+    }
+    const paralelogramArea = paralelogramHeight*paralelogramWidth
+    const paralelogramAreaSpan = document.getElementById('paralelogram-area')
+    paralelogramAreaSpan.innerText = paralelogramArea
+    paralelogramHeightElement.value = ''
+    paralelogramWidthElement.value = ''
 })
